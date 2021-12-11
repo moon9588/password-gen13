21c79ecf32872b325b3d1c3fb3cd85b7be65a5da
moon9588 <moon9588@yahoo.com>
2021-12-11 01:07:11 -0500

    window.prompt

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ function writePassword() {
 
 }
 
+var password = window.alert ("Welcome to Password Generator")
+// Tell users to add a password
+
+var passwordciteria = window.alert( "Password must have minimum of at least 8 characters and no more than 128 characters")
+
+// characters to include options 
+var promptcharacter1 = window.prompt('Would you like to add a lowercase letter? Enter "Yes" or "No" to choose.');
+var promptcharacter2 = window.prompt('Would you like to add a uppercase letter? Enter "Yes" or "No" to choose.');
+var promptcharacter3 = window.prompt('Would you like to add a numberic number? Enter "Yes" or "No" to choose.');
+var promptcharacter4 = window.prompt('Would you like to add a special character ? Enter "Yes" or "No" to choose.');
+
+
+
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
